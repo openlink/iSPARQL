720a18f4bb4b126f1fe1280e071040aea2390885
Patrick van Kleef <pkleef@openlinksw.com>
2019-01-01 21:56:40 +0100

    Updated Copyright to include 2019

--- gen_version.sh
+++ gen_version.sh
@@ -3,7 +3,7 @@
 #  Calculate version number for vad package based on the number of
 #  commits since the last release
 #
-#  Copyright (C) 2018 OpenLink Software
+#  Copyright (C) 2019 OpenLink Software
 #
 
 export LC_ALL=C
